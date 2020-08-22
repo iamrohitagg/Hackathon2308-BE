62b7783f37ff463ff2a670119fc31b7eeaeef009
Ritesh Ranjan <unsortedarray@Riteshs-MBP.Dlink>
2020-08-22 23:11:55 +0530

    Added test cases 3

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ app.use(express.urlencoded());
 app.use(express.json());
 var connection = mysql.createConnection({
 
-    host     : '3.7.227.43',
-    user     : 'root',
-    database : 'hackathon2308',
-    port: 3306
+    host     : DB_END_POINT,
+    user     : DB_USER_NAME,
+    database : DB_DATABASE_NAME,
+    port: DB_PORT_NUM
   });
 
 connection.connect();
@@ -24,55 +24,7 @@ app.get('/', (req, res) => res.send('Hello World!'))
 // your code goes here
 
 // here
-app.get('/issues', (req, res) =>{
-    const page = req.query.page;
-    connection.query("select * from `issues` limit 10 offset 2",function(err, result) {
-        if(err) throw err;
-        //res.json({message: "Successfully fetched 10 records", issues: result})
-        res.json({status: res.status,issues: result })
-    }  )
-    res.send({status:"done"});
-   
-})
 
-app.post('/data', function(req, res){
-    const query = "INSERT INTO `issues` (id, name, description, url, number, label, state, locked ) VALUES (?)"
-    const values = [req.body.id, req.body.name, req.body.description, req.body.url, req.body.number , serialiser.serialize(req.body.label), req.body.state, req.body.locked]
-    connection.query(query, [values], function(err, result){
-        if(err) throw err;
-            console.log("1 record inserted");
-        });
-    res.send({status: "success" , message:"done"});
-});
-
-
-app.patch('/update-issue/:id', (req, res) => {
-
-
-    const  query = "UPDATE `issues` set name=? , description=? , url=?, number=?, label=?, state=?, locked=? where id=?";
-    const  values = [ req.body.name, req.body.description, req.body.url, req.body.number , serialiser.serialize(req.body.label), req.body.state, req.body.locked,  req.body.id];
-
-    connection.query(query, values, (err, result) => {
-        if(err) throw err;
-
-        console.log("1 record updated successfullly");
-    })
-    res.send({status: "success" , message:"done"});
-
-})
-
-app.delete('/delete-issue/:id', (req, res) => {
-    const query = "Delete from `issues` where id=?"
-    const value = parseInt(req.params.id);
-
-    connection.query(query, value, (err, result) => {
-        if(err) throw err;
-        console.log("1 record updated successfullly");
-    })
-
-    res.send({status: "success" , message:"done"});
-
-})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app;
